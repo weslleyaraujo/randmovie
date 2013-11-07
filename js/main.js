6a11d210fff0e10a2af82067a73e4d4722760024
Weslley Araujo <weslleyaraujo20@gmail.com>
2013-11-07 00:42:14 -0200

    irb

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+var randMovie = (function () {
+	var _app, _private;
 
+	_app = {
+		show: function () {
+			this.$logo.addClass('is-positioned');
+			this.$movieIcon.addClass('is-visible');
+			this.$content.addClass('is-visible');
+		},
+
+		init: function (){
+			this.dom();
+			this.bind();
+			return this;
+		},
+
+		dom: function (){
+			this.$logo = $('.logo');
+			this.$movieIcon = $('.logo-movie-icon');
+			this.$content = $('.fade-in-content');
+		},
+
+		bind: function (){
+
+		}
+	};
+
+	return _app;
+
+}());
