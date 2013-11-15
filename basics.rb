218d5e6c57e883d4b857363e73937f66686aa7a4
Weslley Araujo <weslleyaraujo20@gmail.com>
2013-11-15 15:00:29 -0200

    rand api is working :)

--- basics.rb
+++ basics.rb
@@ -1,7 +1,26 @@
 require 'sinatra'
 require 'shotgun'
+require 'mongo'
+require 'json/ext'
 
-get '/api' do
+include Mongo
+
+# mongo config
+configure do
+	conect = Mongo::Connection.new
+	set :mongo_connection, conect
+	set :mongo_db, conect.db('randmovie')
+end
+
+get '/api/get/*' do
+	params['captures'].delete('')
+	if params['captures'].empty?
+		countdb = settings.mongo_db['movies'].count()
+		movie = settings.mongo_db['movies'].find().limit(-1).skip(rand(countdb)).next_document()
+		movie.to_json
+	else
+		'search for ' + params['captures'].first.to_s
+	end
 end
 
 get '/' do
